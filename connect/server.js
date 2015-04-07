83a55877e4084c772f1fdb874a87e36e0c7ceea9
xyclr <xyclr@163.com>
2015-04-07 16:28:34 +0800

    s

--- connect/server.js
+++ connect/server.js
@@ -1,46 +1,12 @@
-/**
- * Created by cdgoujianjun on 2015/3/12.
- */
 var connect = require('connect');
-var app = connect();
-app.use(logger);
-app.use('/admin', restrict);
-app.use('/admin', admin);
-app.use(hello);
-app.listen(3000)
 
-function logger (req, res, next){
-    console.log('%s %s', req.method, req.url);
+function logger(req, res, next){
+    console.log('%s %s', req.method, req, req.url);
     next();
 }
-
-function restrict(req, res, next){
-    var authorization = req.headers.authorization;
-    if(!authorization) return next(new Error('Unauthorized'));
-    var parts = authorization.split(' ');
-    var scheme = parts[0];
-    var auth = new Buffer(parts[1], 'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
-
-    authenticateWidthDatabase(user, pass, function(err){
-        if(err) return newt(err);
-        next();
-    })
-}
-
-function admin(req, res, next){
-    switch (req.url){
-        case '/' :
-            res.end('try /users');
-            break;
-        case '/users' :
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify(['tobi', 'loki', 'jane']));
-    }
-}
-
 function hello(req, res){
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!!!');
+    res.end('hello word');
 }
+
+connect().use(logger).use(hello).listen(3000);
